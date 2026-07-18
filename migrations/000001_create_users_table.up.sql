241c847433686740b2dc48ebc7da22d793f38e09
mbarrah <mohamedbarrahx@gmail.com>
2026-07-18 01:56:25 +0100

    the user table

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -4,6 +4,16 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    gender TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,  
+    is_public INTEGER NOT NULL DEFAULT 1,
+    password TEXT NOT NULL,
+    
+    avatar TEXT,                  
+    nick_name TEXT,
+    about_me TEXT,
+    
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);  
+);
